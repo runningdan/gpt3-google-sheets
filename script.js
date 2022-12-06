c799c86e2d650cd3084a37ffbe97f47b1b6ef8eb
runningdan <dan@cloudaro.io>
2022-12-05 22:16:02 -0600

    added more comments & made code more readable

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /* Simple Google Docs GPT3 integration created by Dan White */
 
 // Attach your api-key
-var API_KEY = "<OPENAI_API_KEY>";
+var OPENAI_API_KEY = "<OPENAI_API_KEY>";
 
 // Set max num tokens for response
-const NUM_TOKENS = 64;
+const MAX_TOKENS = 64;
 
 // function which we can call gpt-3 api
 function callAPI(prompt) {
@@ -12,30 +12,33 @@ function callAPI(prompt) {
     "model": "text-davinci-003",
     "prompt": prompt,
     "temperature": 0.7,
-    "max_tokens": NUM_TOKENS,
+    "max_tokens": MAX_TOKENS,
     "top_p": 1,
     "frequency_penalty": 0,
     "presence_penalty": 0
   };
 
-  var options = {
+  var payload = {
     'method' : 'post',
     'contentType': 'application/json',
     'payload' : JSON.stringify(data),
     'headers': {
-      Authorization: 'Bearer ' + API_KEY,
+      Authorization: 'Bearer ' + OPENAI_API_KEY,
     },
   };
 
-  response = UrlFetchApp.fetch(
+  var response = UrlFetchApp.fetch(
     'https://api.openai.com/v1/completions',
-    options,
+    payload,
   );
-  
-  return JSON.parse(response.getContentText())['choices'][0]['text']
+    
+  var choice = JSON.parse(response.getContentText())['choices'][0]['text']
+
+  return choice
 }
 
-
+// function GPT3(arg1, arg2, ...) can be called within your spread sheet
+// The function concatenates the inputs to create a prompt which is sent to GPT3
 function GPT3() {
 
   // create prompt for gpt-3
