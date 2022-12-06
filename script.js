ec49c0c54f9dcce73105dbcc6ee73430bae95c79
runningdan <dan@cloudaro.io>
2022-12-05 20:43:49 -0600

    added comments and trim

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var API_KEY = "<OPENAI_API_KEY>"; // add your api key here
-var NUM_TOKENS = 64;
+/* Simple Google Docs GPT3 integration created by Dan White */
+
+// Attach your api-key
+var API_KEY = "<OPENAI_API_KEY>";
+
+// Set max num tokens for response
+const NUM_TOKENS = 64;
+
+// function which we can call gpt-3 api
 function callAPI(prompt) {
   var data = {
     "model": "text-davinci-003",
@@ -10,6 +17,7 @@ function callAPI(prompt) {
     "frequency_penalty": 0,
     "presence_penalty": 0
   };
+
   var options = {
     'method' : 'post',
     'contentType': 'application/json',
@@ -18,14 +26,27 @@ function callAPI(prompt) {
       Authorization: 'Bearer ' + API_KEY,
     },
   };
+
   response = UrlFetchApp.fetch(
     'https://api.openai.com/v1/completions',
     options,
   );
+  
   return JSON.parse(response.getContentText())['choices'][0]['text']
 }
+
+
 function GPT3() {
-  prompt = Array.prototype.slice.call(arguments, 0).join(' ')
+
+  // create prompt for gpt-3
+  var prompt = Array.prototype.slice.call(arguments, 0).join(' ').trim()
+  if (prompt == "") {
+    return "Error Empty Input"
+  }
+
+  // call open-ai with prompt
   var response = callAPI(prompt)
+
+  // return gpt-3 response
   return response
 }
